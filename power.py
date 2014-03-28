457c95f1520804679c800714cbcc981fa3c13ae9
Bruno Fagundez <bfagundez@gmail.com>
2014-03-28 11:14:50 -0300

    tested endpoints

--- power.py
+++ power.py
@@ -1,66 +1,46 @@
+# add flask here
+from flask import Flask
+app = Flask(__name__)
+app.debug = True
+# keep your code
 import time
 import cgi
-form = cgi.FieldStorage()
- 
-print "Content-Type: text/html"
-print
-print '<!doctype html>'
-print '<title>Power</title>'
- 
-if not ("device" in form and "power" in form and "pass" in form):
-    print "<p class=error> Incorrect params have been provided."
-    print "<script>" 
-    print "function goBack()"
-    print "{"
-    print "window.history.back()"
-    print "}"
-    print "</script>"
-    print "<body>"
-    print """<button onclick="goBack()">Go Back</button>"""
-    print "</body>"
-    exit() #end here if errors
- 
-device_id, power, powerOnTime = int(form['device'].value), form['power'].value, int(form['time'].value)
- 
-if not ('pass' in form and form['pass'].value == "p@ssw0rd"): 
-	print "<p class=errr> Password is incorrect."
-        exit() 
-
 from tellcore.telldus import TelldusCore
 core = TelldusCore()
 devices = core.devices()
- 
-if not (power in ['off','on','time'] and
-        device_id <= len(devices)):
-        print "<p class=err> Incorrect values for the params have been provided."
-        print "<script>"
-        print "function goBack()"
-        print "{"
-        print "window.history.back()"
-        print "}"
-        print "</script>"
-        print "<body>"
-        print """<button onclick="goBack()">Go Back</button>"""
-        print "</body>"
-        exit() #end here if errors
-
 device = devices[device_id]
-if power == 'on':
-    device.turn_on()
-if power == 'off':
-    device.turn_off()
 
-if powerOnTime > 0:
-    device.turn_on()
-    time.sleep(powerOnTime)
-    device.turn_off()
-print '<p class=success> Device ID %s has been turned %s for %s seconds' % (device_id,power,powerOnTime)
-print "<script>"
-print "function goBack()"
-print "{"
-print "window.history.back()"
-print "}"
-print "</script>"
-print "<body>"
-print """<button onclick="goBack()">Go Back</button>"""
-print "</body>"
+# define a "power ON api endpoint"
+@app.route("/API/v1.0/power-on/<deviceId>",methods=['POST'])
+def powerOnDevice(deviceId):
+    payload = {}
+    #get the device by id somehow
+    device = devices[deviceId]
+    # get some extra parameters 
+    # let's say how long to stay on
+    params = request.get_json()
+    try:
+      device.turn_on()
+      payload['success'] = True
+      return payload
+    except:
+      payload['success'] = False
+      # add an exception description here
+      return payload
+
+# define a "power OFF api endpoint"
+@app.route("/API/v1.0/power-off/<deviceId>",methods=['POST'])
+def powerOffDevice(deviceId):
+    payload = {}
+    #get the device by id somehow
+    device = devices[deviceId]
+    try:
+      device.turn_off()
+      payload['success'] = True
+      return payload
+    except:
+      payload['success'] = False
+      # add an exception description here
+      return payload
+
+app.run()
